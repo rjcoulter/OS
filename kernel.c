994cb8006be980518ab9112ec6c87e91ff4dcb76
rjcoulter <rjc8ez@virginia.edu>
2019-01-04 19:02:05 -0500

    Add kernel, still need to configure gcc

---
+++ kernel.c
@@ -0,0 +1,8 @@
+/* Forces us to create a kernel entry function */
+void dummy_test_entrypoint() {
+}
+
+void main() {
+	char* video_memory = (char*) 0xb8000;
+	*video_memory = 'X';
+}
---
+++ kernel_entry.asm
@@ -0,0 +1,4 @@
+[bits 64]
+[extern main] ; Defines calling point
+call main ; Calls C function
+jmp $
